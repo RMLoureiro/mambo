db45d01ab3b2f61a33aad4035f759d771987387b
RMLoureiro <rf.loureiro@campus.fct.unl.pt>
2020-04-15 03:43:20 +0100

    bug in babel fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,10 +9,10 @@ public class Main {
 
     /**
      Activate logger
-    **/
     static {
         System.setProperty("log4j.configurationFile", "log4j2.xml");
     }
+    **/
 
     public static void main(String[] args) throws Exception {
         Babel babel = Babel.getInstance();

0a27b7de4d2e7c94ef2f6c7e2526ce4e78cd1aa9
RMLoureiro <rf.loureiro@campus.fct.unl.pt>
2020-05-21 19:35:43 +0100

    first chord commit

---
+++ src/main/java/Memberships/Chord/Chord.java
@@ -0,0 +1,22 @@
+package Memberships.Chord;
+
+import babel.exceptions.HandlerRegistrationException;
+import babel.generic.GenericProtocol;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class Chord extends GenericProtocol {
+
+    public static short PROTOCOL_ID = 200;
+    public static String PROTOCOL_NAME = "CHORD";
+
+    public Chord() {
+        super(PROTOCOL_NAME, PROTOCOL_ID);
+    }
+
+    @Override
+    public void init(Properties properties) throws HandlerRegistrationException, IOException {
+
+    }
+}
